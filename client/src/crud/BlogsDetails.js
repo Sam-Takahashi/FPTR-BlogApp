cbd34f4e8890b7d0baede78aa84ffbb7a209befe
Sam Takahashi <sam.takahashi.sam@gmail.com>
2022-05-07 13:29:13 +0900

    changed run command script

--- client/src/crud/BlogsDetails.js
+++ client/src/crud/BlogsDetails.js
@@ -3,11 +3,11 @@ import useFetch from "../myHooks/useFetch";
 
 const BlogDetails = () => {
     const { theId } = useParams();
-    const { data: blogData, error, isLoading } = useFetch('https://react-blog-app-zeta.vercel.app/blog/' + theId);
+    const { data: blogData, error, isLoading } = useFetch('http://localhost:3000/blog/' + theId);
     const history = useNavigate();
 
     const handleClick = () => {
-        fetch('https://react-blog-app-zeta.vercel.app/delete-blog/' + blogData.id, {
+        fetch('http://localhost:3000/delete-blog/' + blogData.id, {
             method: 'DELETE'
         }).then(() => {
             history('/');
